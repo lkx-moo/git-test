6c7b5aa40a7d62fc0aded2277fe4769505756a81
lkx <lkx@163.com>
2021-10-25 20:56:05 +0800

    second commit

--- src/com/lkx/git/GitTest.java
+++ src/com/lkx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lkx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 01");
+        System.out.println("hello git 02");
     }
 }
