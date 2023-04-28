3d1259532a3c0edb24457d33a12a39cb0d74466d
lkx <lkx@163.com>
2023-04-28 17:52:18 +0800

    revert-335a333

--- src/com/lkx/git/GitTest.java
+++ src/com/lkx/git/GitTest.java
@@ -1,6 +1,7 @@
 public class GitTest{
 	System.out.println("git 001 lkx");
 	System.out.println("git 001 master");
+	System.out.println("MMMMMMMMMMMMMM-MASTER");
 	System.out.println("AAAAAAAAAAAAAA-LKX");
 	System.out.println("BBBBBBBBBBBBBB-LKX");
 }
