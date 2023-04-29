a9a9cd263b4019f4ee4080a9ab45b6175eb6a57b
lkx <lkx@163.com>
2023-04-29 16:02:00 +0800

    ADD-B-LKX

--- src/com/lkx/git/GitTest.java
+++ src/com/lkx/git/GitTest.java
@@ -1,8 +1,6 @@
 public class GitTest{
 	System.out.println("git 001 lkx");
 	System.out.println("MMMMMMMMMMMMMM-MASTER");
-	System.out.println("FFFFFFFFFFFFFF-MASTER");
-	System.out.println("git 001 master");
 	System.out.println("AAAAAAAAAAAAAA-LKX");
-	System.out.println("MMMMMMMMMMMMMM-LKX");
+	System.out.println("BBBBBBBBBBBBBB-MASTER");
 }
